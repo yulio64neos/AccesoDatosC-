737593d5f4f8dcc9397418110391683cd304cc23
Andres381 <utp0138881@alumno.utpuebla.edu.mx>
2022-06-17 07:58:55 -0500

    Se creo la conexión SQL SERVER

---
+++ WebPruebaAccesoDat/WebForm1.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebPruebaAccesoDat.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Adobo .NET</h1>
+            <asp:Button ID="btnDatos" runat="server" Text="Probar acceso a datos" OnClick="btnDatos_Click" />
+            <br />
+            <asp:TextBox ID="TextBox1" runat="server" Width="1075px"></asp:TextBox>
+            <br />
+            <asp:Button ID="btnProfe" runat="server" Text="Insertar Profesor" OnClick="btnProfe_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebPruebaAccesoDat/WebForm1.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace WebPruebaAccesoDat
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnDatos_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection camino = new SqlConnection())
+            {
+                camino.ConnectionString = System.Configuration.ConfigurationManager.
+                    ConnectionStrings["conlocal"].ConnectionString;
+                try
+                {
+                    camino.Open();
+                    TextBox1.Text = "Conexión establecida correctamente";
+                }
+                catch (Exception a)
+                {
+                    TextBox1.Text = "BUGASO: " + a.Message;
+                }
+            }
+        }
+
+        protected void btnProfe_Click(object sender, EventArgs e)
+        {
+            SqlCommand bocho = null;
+            using (SqlConnection camino = new SqlConnection())
+            {
+                camino.ConnectionString = System.Configuration.ConfigurationManager.
+                    ConnectionStrings["conlocal"].ConnectionString;
+                try
+                {
+                    camino.Open();
+                    bocho = new SqlCommand();
+                    bocho.CommandText = "Insert Into Profesor(RegistroEmpleado, Nombre, Ap_pat, Ap_mat, Genero, Categoria, F_edoCivil)" +
+                        "values(245, 'Pedro', 'Martinez', 'Galaviz', 'Masculino', 'PTC', 12);";
+                    TextBox1.Text = "Conexión establecida correctamente";
+                    bocho.Connection = camino;
+                    bocho.ExecuteNonQuery();
+                    TextBox1.Text = "Inserción correcta";
+                }
+                catch (Exception a)
+                {
+                    TextBox1.Text = "NELSON: " + a.Message;
+                }
+            }
+        }
+    }
+}
